7850dfd54ef06547e9cfdd7d10de1d37d1b3aa4e
lb26227 <lillian.ballantine@uvm.edu>
2025-10-16 14:17:50 -0400

    Added sprint1 files

---
+++ main.py
@@ -0,0 +1,20 @@
+import classes
+import arcade
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "Starting Screen"
+
+"""
+main function
+"""
+def main():
+    """ Main function """
+    # Create a window class. This is what actually shows up on scree
+    window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, "Different Views Minimal Example")
+    menu_view = classes.MenuView()
+    window.show_view(menu_view)
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
